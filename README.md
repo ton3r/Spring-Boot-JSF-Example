4a3f15dc8de97d1c61739937a10a0ec444fa6215
Tom Kornelson <tk@to3.de>
2017-09-08 11:01:45 +0200

    disable/remove theme settings since them do not work proper and not neccessary for this demo, in pom.xml and JSFConfiguration
    describe the problem in README.md
    
    Change Spring Boot Version to 1.5.3.RELEASE

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 Spring-Boot-JSF-Example
 =======================
 
-Fork of Zergleb's Spring Boot + JSF Integration
+Fork of Zergleb's Spring Boot + JSF Integration, forked from cenobyte321/Spring-Boot-JSF-Example 
+
+
+# This is a demonstration that there is probably a _bug/error/feature/_ during the creation of an executable .war file with spring-boot-maven-plugin since Version >= 1.5.3.RELEASE
+
+Build it with
+
+_mvn clean install spring-boot:repackage_
+
+and try to execute it with
+
+_java -jar target/application-0.0.1-SNAPSHOT.war_
+
+You will get during startup an Exception like this :
+
+
+*2017-09-08 10:48:23.860  INFO 35369 --- [ost-startStop-1] j.e.resource.webcontainer.jsf.config     : Mojarra 2.2.7 ( 20140610-1547 https://svn.java.net/svn/mojarra~svn/tags/2.2.7@13362) für Kontext '' wird initialisiert.*
+
+*2017-09-08 10:48:27.938 ERROR 35369 --- [ost-startStop-1] j.e.resource.webcontainer.jsf.config     : Critical error during deployment:* 
+
+*com.sun.faces.config.ConfigurationException: java.util.concurrent.ExecutionException: javax.faces.FacesException: java.io.FileNotFoundException: JAR entry META-INF/ not found in /var/folders/gj/...*
+
+
+Changing spring-boot-parent version in pom to something lower 1.5.3.RELEASE it works fine.
+
+
+
+
+
+###Original Readme
 
 ##Changes##
 
--- src/main/java/application/configuration/jsf/JSFConfiguration.java
+++ src/main/java/application/configuration/jsf/JSFConfiguration.java
@@ -1,7 +1,8 @@
+
 package application.configuration.jsf;
+
 import java.util.EnumSet;
 import java.util.HashMap;
-
 
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.DispatcherType;
@@ -15,114 +16,107 @@ import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 import com.sun.faces.config.ConfigureListener;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 /**
  * Created by anonymous on 1/31/16.
  */
 @Configuration
 public class JSFConfiguration {
-    /**
-     * Configuración de ViewScope
-     *
-     * @return ViewScope
-     */
-    @Bean
-    public static ViewScope viewScope() {
-        return new ViewScope();
-    }
+	/**
+	 * Configuración de ViewScope
+	 *
+	 * @return ViewScope
+	 */
+	@Bean
+	public static ViewScope viewScope() {
+		return new ViewScope();
+	}
 
-    /**
-     * Allows the use of @Scope("view") on Spring @Component, @Service and @Controller
-     * beans
-     */
-    @Bean
-    public static CustomScopeConfigurer scopeConfigurer() {
-        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-        HashMap<String, Object> hashMap = new HashMap<String, Object>();
-        hashMap.put("view", viewScope());
-        configurer.setScopes(hashMap);
-        return configurer;
-    }
+	/**
+	 * Allows the use of @Scope("view") on Spring @Component, @Service and @Controller
+	 * beans
+	 */
+	@Bean
+	public static CustomScopeConfigurer scopeConfigurer() {
+		CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+		HashMap<String, Object> hashMap = new HashMap<String, Object>();
+		hashMap.put("view", viewScope());
+		configurer.setScopes(hashMap);
+		return configurer;
+	}
 
-    /*
-     * Below sets up the Faces Servlet for Spring Boot
-     */
-    @Bean
-    public FacesServlet facesServlet() {
-        return new FacesServlet();
-    }
+	/*
+	 * Below sets up the Faces Servlet for Spring Boot
+	 */
+	@Bean
+	public FacesServlet facesServlet() {
+		return new FacesServlet();
+	}
 
-    /**
-     * Configuración de JSF
-     *
-     * @return
-     */
-    @Bean
-    public ServletRegistrationBean facesServletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(
-                facesServlet(), "*.xhtml");
-        registration.setName("FacesServlet");
-        registration.setLoadOnStartup(1);
-        return registration;
-    }
+	/**
+	 * Configuración de JSF
+	 *
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				facesServlet(), "*.xhtml");
+		registration.setName("FacesServlet");
+		registration.setLoadOnStartup(1);
+		return registration;
+	}
 
-    /**
-     * Configuración de JSF
-     *
-     * @return
-     */
-    @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<ConfigureListener>(
-                new ConfigureListener());
-    }
+	/**
+	 * Configuración de JSF
+	 *
+	 * @return
+	 */
+	@Bean
+	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<ConfigureListener>(
+				new ConfigureListener());
+	}
 
-    /**
-     * Configuración de servlet startup de JSF
-     *
-     * @return
-     */
-    @Bean
-    public ServletContextInitializer initializer() {
-        return new ServletContextInitializer() {
+	/**
+	 * Configuración de servlet startup de JSF
+	 *
+	 * @return
+	 */
+	@Bean
+	public ServletContextInitializer initializer() {
+		return new ServletContextInitializer() {
 
-            @Override
-            public void onStartup(ServletContext servletContext)
-                    throws ServletException {
+			@Override
+			public void onStartup(ServletContext servletContext)
+					throws ServletException {
 
-                //Los parámetros del servlet (web.xml) son agregados aquí
-                //De todas formas se debe de dejar el archivo web.xml para que no arroje errores JSF
+				// Los parámetros del servlet (web.xml) son agregados aquí
+				// De todas formas se debe de dejar el archivo web.xml para que no arroje errores JSF
 
-                //Asignamos el tema de Bootstrap
-                servletContext.setInitParameter("primefaces.THEME",
-                        "bootstrap");
-                servletContext.setInitParameter("javax.faces.INTERPRET_EMPTY_STRING_SUBMITTED_VALUES_AS_NULL","true");
+				servletContext.setInitParameter("javax.faces.INTERPRET_EMPTY_STRING_SUBMITTED_VALUES_AS_NULL", "true");
 
-                //Configuración de taglibs con Spring Security
-                servletContext.setInitParameter(
-                        "com.sun.faces.forceLoadConfiguration", "true");
-                servletContext.setInitParameter(
-                        "javax.faces.FACELETS_LIBRARIES",
-                        "/WEB-INF/springsecurity.taglib.xml");
-                servletContext.setInitParameter("javax.faces.FACELETS_LIBRARIES", "/WEB-INF/springsecurity.taglib.xml");
+				// Configuración de taglibs con Spring Security
+				servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", "true");
+				servletContext.setInitParameter("javax.faces.FACELETS_LIBRARIES", "/WEB-INF/springsecurity.taglib.xml");
 
-                EnumSet<DispatcherType> tiposDispatcher = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-                servletContext.addFilter("securityFilter",
-                        new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(tiposDispatcher, false, "/*");
+				EnumSet<DispatcherType> tiposDispatcher = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+				servletContext.addFilter("securityFilter",
+						new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(tiposDispatcher, false, "/*");
 
-                //Configuración del filter upload de Primefaces
-                servletContext.setInitParameter("primefaces.UPLOADER", "commons");
-                servletContext.addFilter("PrimeFaces FileUpload Filter", new FileUploadFilter())
-                        .addMappingForServletNames(tiposDispatcher, false, "FacesServlet");
+				// Configuración del filter upload de Primefaces
+				servletContext.setInitParameter("primefaces.UPLOADER", "commons");
+				servletContext.addFilter("PrimeFaces FileUpload Filter", new FileUploadFilter())
+						.addMappingForServletNames(tiposDispatcher, false, "FacesServlet");
 
-                //ViewExpiredException filter register
-                servletContext.addFilter("errorHandlerFilter", new ViewExpiredExceptionFilter())
-                        .addMappingForUrlPatterns(tiposDispatcher,false, "/*");
+				// ViewExpiredException filter register
+				servletContext.addFilter("errorHandlerFilter", new ViewExpiredExceptionFilter())
+						.addMappingForUrlPatterns(tiposDispatcher, false, "/*");
 
-            }
-        };
-    }
+			}
+		};
+	}
 }
